8e77ed519caba5444da0760c96fd8a63b91fe967
Bryan Bell <bryanbellabc@gmail.com>
2021-08-12 11:55:25 -0600

    Estructura inicial - Operaciones de Lectura

---
+++ src/App.svelte
@@ -0,0 +1,33 @@
+<script>
+	import { firebaseApp } from './firebase'
+	import { getFirestore, collection, getDocs, getDoc, doc } from 'firebase/firestore/lite';
+
+
+	const db = getFirestore(firebaseApp);
+	
+	let data = [];
+	$: console.log(data)
+
+	async function getData() {
+
+		/* PETICIÓN A COLECCIONES */
+			const col = collection(db, "users");
+			const querySnapshot = await getDocs(col);
+				data = querySnapshot.docs
+
+		/* PETICIÓN A DOCUMENTOS ESPECÍFICOS */
+			const docREF = doc(db, 'users/7JfXt85QrggYlVFLlb75');
+			const snapshot = await getDoc(docREF);
+				console.log(snapshot.data())
+
+	}
+
+	getData()
+
+
+
+	/* const snapshot = await getDocs(collection('users')); */
+	  /*  */
+</script>
+
+<h1>Hello World</h1>
